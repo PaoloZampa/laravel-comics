61b2e1942e9cd97c844a2d9de9d6a209fe8ff408
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-05-23 16:23:46 +0200

    add logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,37 +1,41 @@
-<!-- Nav tabs -->
-<ul class="nav nav-tabs" id="navId">
-    <li class="nav-item">
-        <a href="#tab1Id" class="nav-link active">Active</a>
-    </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#tab2Id">Action</a>
-            <a class="dropdown-item" href="#tab3Id">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#tab4Id">Action</a>
-        </div>
-    </li>
-    <li class="nav-item">
-        <a href="#tab5Id" class="nav-link">Another link</a>
-    </li>
-    <li class="nav-item">
-        <a href="#" class="nav-link disabled">Disabled</a>
-    </li>
-</ul>
-
-<!-- Tab panes -->
-<div class="tab-content">
-    <div class="tab-pane fade show active" id="tab1Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab2Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab3Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab4Id" role="tabpanel"></div>
-    <div class="tab-pane fade" id="tab5Id" role="tabpanel"></div>
-</div>
-
-<script>
-    $('#navId a').click(e => {
-        e.preventDefault();
-        $(this).tab('show');
-    });
-</script>
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
+    <div class="logo">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+    </div>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/comics">Link 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 3</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 4</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 5</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 6</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 7</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 8</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 9</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link 10</a>
+        </li>
+    </ul>
+    <form class="form-inline ml-auto d-flex">
+        <input class="form-control mr-sm-2 border-0 shadow" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn border-0" type="submit">Search</button>
+    </form>
+</nav>
---
+++ resources/views/partials/jumbotron.blade.php
@@ -0,0 +1,3 @@
+<section class="jumbotron">
+    <img src="{{ Vite::asset('resources/img/jumbotron.png') }}" alt="">
+</section>
